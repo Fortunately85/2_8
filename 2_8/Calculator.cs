32fd45ff998c66b51aba097bea667454d7f30af6
asus <asus@LAPTOP-KM394PSE>
2020-03-16 21:23:00 +0800

    添加项目文件。

---
+++ 2_8/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_8
+{
+    class Calculator
+    {
+        public string a;
+        public string b;
+        public string c;
+        public int e;
+        public int f;
+        public void setmessage()
+        {
+            Console.WriteLine("请输入第一个数字或字符串：");
+            a = Console.ReadLine();
+            Console.WriteLine("请输入运算符（+ - * /） ：");
+            c = Console.ReadLine();
+            Console.WriteLine("请输入第二个数字或字符串：");
+            b = Console.ReadLine();
+        }
+        public void jisuan()
+        {
+            float d;
+            if (c == "+")
+            {
+                d = e + f;
+                Console.WriteLine(a + c + b + '=' + d);
+            }
+
+            else
+                if (c == "-")
+            {
+                d = e - f;
+                Console.WriteLine(a + c + b + '=' + d);
+            }
+
+            else
+                if (c == "*")
+            {
+                d = e * f;
+                Console.WriteLine(a + c + b + '=' + d);
+            }
+
+            else
+                if (c == "/")
+            {
+                d = e / f;
+                Console.WriteLine(a + c + b + '=' + d);
+            }
+            else
+                Console.WriteLine("符号输入错误");
+        }
+    }
+}
